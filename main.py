da44f9baf02bcee5e08e71e0a9acb36dcc67b1ce
rgonzal92 <rgonzal92@gmail.com>
2021-03-28 00:02:48 +0000

    added next() function, date sorting

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 import random
 import json
 
+from datetime import datetime
 from discord.ext import commands
 from dotenv import load_dotenv
 
-data_file = "data/test.json"
+data_file = "data/data.json"
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,16 +15,35 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.command(name='test', help="Debug: testing courses.")
-async def name(ctx):
-    user_name = ctx.message.author.name
-    with open(data_file) as f:
-        data = json.load(f)
-    if user_name in data:
-        for val in data[user_name].values():
-            await ctx.send(f"{val['name']}, Section {val['section']} on {val['days']}")
-    else:
-        await ctx.send("Sorry, I could not find you in the database.")
+@bot.command(name='next', help="Gets the next upcoming assignment(s).")
+async def next(ctx, course_num):
+    found = False
 
+    # search through all channels and
+    # match channel name with channel_id
+    for channel in ctx.guild.channels:
+        if channel.name == course_num:
+            channel_id = str(channel.id)
+            found = True
+
+    while(found):
+        # open file
+        with open(data_file) as f:
+            data = json.load(f)
+
+        # sort date
+        list_of_dates = []
+        for date in data[channel_id]['due_dates']:
+            list_of_dates.append(date)
+        list_of_dates.sort(key=lambda date: datetime.strptime(date, '%m/%d/%y'))
+
+        # check for assignments
+        if channel_id in data:
+            for assignment in data[channel_id]['due_dates'][list_of_dates[0]]:
+                await ctx.send(f"Next assignment: {assignment} on {list_of_dates[0]}")
+            return None
+
+    # no course exist
+    await ctx.send("Sorry, course does not exist.")
 
 bot.run(TOKEN)
